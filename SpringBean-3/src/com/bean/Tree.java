f11b2f84559e71d26114ed1bfc9fe98cc07a7132
kalpesh <kalpesh162@gmail.com>
2026-05-22 16:12:13 +0530

    spring Bean -3

--- SpringBean-3/src/com/bean/Tree.java
+++ SpringBean-3/src/com/bean/Tree.java
@@ -3,9 +3,6 @@ package com.bean;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
 public class Tree {
 	private int ht;
 	private String name;
