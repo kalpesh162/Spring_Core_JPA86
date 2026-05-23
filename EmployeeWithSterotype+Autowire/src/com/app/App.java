04e1b45acae2fc5c7ea333727bf4ed680da9e462
kalpesh <kalpesh162@gmail.com>
2026-05-23 09:36:25 +0530

    Spring Sterotype annotation and autowired annoatation

---
+++ EmployeeWithSterotype+Autowire/src/com/app/App.java
@@ -0,0 +1,21 @@
+package com.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bean.Employee;
+
+public class App {
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+		Employee employee1 = (Employee) context.getBean("employee");
+
+		System.out.println(employee1);
+
+		System.out.println(employee1.getAddress());
+
+	}
+
+}
---
+++ EmployeeWithSterotype+Autowire/src/com/bean/Address.java
@@ -0,0 +1,62 @@
+package com.bean;
+
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Address implements Serializable {
+
+	@Value("FC ROAD")
+	private String landMark;
+	@Value("PUNE")
+	private String city;
+	@Value("444222")
+	private int pincode;
+
+	public Address() {
+		System.out.println("Address Default Constructor");
+
+	}
+
+	public Address(String landMark, String city, int pincode) {
+		System.out.println("Address Parameterized Constructor");
+		this.landMark = landMark;
+		this.city = city;
+		this.pincode = pincode;
+	}
+
+	public String getLandMark() {
+		return landMark;
+	}
+
+	public void setLandMark(String landMark) {
+		System.out.println("Set LandMark");
+		this.landMark = landMark;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		System.out.println("Set city");
+		this.city = city;
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		System.out.println("SET PINCODE");
+		this.pincode = pincode;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [landMark=" + landMark + ", city=" + city + ", pincode=" + pincode + "]";
+	}
+
+}
