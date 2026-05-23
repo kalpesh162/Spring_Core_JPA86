0b151c910a77399a77b3066db3fd935b24cbacac
kalpesh <kalpesh162@gmail.com>
2026-05-23 09:09:27 +0530

    Spring Sterotype annotation

---
+++ EmployeeWithSterotypeAnotation-1/src/com/bean/Employee.java
@@ -0,0 +1,60 @@
+package com.bean;
+
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee implements Serializable {
+	@Value("11")
+	private int id;
+	@Value("Kalpesh")
+	private String name;
+	@Value("11111")
+	private double salary;
+
+	public Employee() {
+		System.out.println("Employee Default COstructor Called");
+	}
+
+	public Employee(int id, String name, double salary) {
+		System.out.println("Employee Parameterized COstructor Called");
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		System.out.println("SET ID");
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println("SET NAME");
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		System.out.println("SET SALARY");
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+}
